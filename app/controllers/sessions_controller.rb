51527940a8db2d56ac91cbd85fd12e52d596dac6
LeeinUITk17 <22521276@gm.uit.edu.vn>
2025-07-23 16:26:58 +0700

    chapter_9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      reset_session
-      log_in user
+      log_in_and_remember(user)
       redirect_to user
     else
       flash.now[:danger] = t(".invalid_combination")
@@ -14,7 +13,15 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def log_in_and_remember user
+    reset_session
+    log_in user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,61 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   has_one_attached :avatar
   has_secure_password
+
   enum gender: {female: 0, male: 1, other: 2}
+
   before_save{email.downcase!}
+
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+
   validate :birthday_in_the_past_100_years
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost:)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   private
+
   def birthday_in_the_past_100_years
     return if birthday.blank?
 
     if birthday > Time.zone.today
-      errors.add(:birthday,
-                 "can't be in the future")
+      errors.add(:birthday, "can't be in the future")
     end
+
     return unless birthday < 100.years.ago.to_date
 
-    errors.add(:birthday,
-               "must be within the last 100 years")
+    errors.add(:birthday, "must be within the last 100 years")
   end
 end
